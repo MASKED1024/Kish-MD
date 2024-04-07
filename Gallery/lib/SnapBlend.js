d1bd1a08463706fa470f3729841e71dfbd491c7e
Brashokish <130168063+Brashokish@users.noreply.github.com>
2024-04-07 11:37:50 +0300

    𝙆𝙞𝙨𝙝-𝙈𝙙 𓅃

---
+++ Gallery/lib/SnapBlend.js
@@ -0,0 +1,16 @@
+const prefix = global.prefa
+  const pushname = m.pushName || "No Name"
+
+const reactmenu=`
+╭─_*✍️EDIT✍️*_
+ │ ${prefix}write
+ │ ${prefix}burnpaper
+ │ ${prefix}romatic
+ │ ${prefix}coffecup
+ │ ${prefix}undergrass
+ │ ${prefix}love
+ │ ${prefix}narutobanner
+ ╰─────────────────
+`
+ module.exports = reactmenu
+ 
